52cd16ea365ddab3796cf6fe601dc5dd7304a664
Edwin Samodra <48350243+edsamodra@users.noreply.github.com>
2025-01-07 19:13:17 +0700

    fix(auth): convert apiUtils.generateJWTToken from callback to Promise
    - Transform callback-style function to return Promise
    - Fix error "Cannot read properties of undefined (reading 'token')"
    - Resolve promise compatibility issue with async/await in login controller

--- src/controllers/api/apiUtils.js
+++ src/controllers/api/apiUtils.js
@@ -30,36 +30,41 @@ apiUtils.sendApiError_InvalidPostData = function (res) {
   return apiUtils.sendApiError(res, 400, 'Invalid Post Data')
 }
 
-apiUtils.generateJWTToken = function (dbUser, callback) {
-  const nconf = require('nconf')
-  const jwt = require('jsonwebtoken')
+apiUtils.generateJWTToken = function (dbUser) {
+  return new Promise((resolve, reject) => {
+    const nconf = require('nconf')
+    const jwt = require('jsonwebtoken')
 
-  const resUser = _.clone(dbUser._doc)
-  const refreshToken = resUser.accessToken
-  delete resUser.resetPassExpire
-  delete resUser.resetPassHash
-  delete resUser.password
-  delete resUser.iOSDeviceTokens
-  delete resUser.tOTPKey
-  delete resUser.__v
-  delete resUser.preferences
-  delete resUser.accessToken
-  delete resUser.deleted
-  delete resUser.hasL2Auth
+    const resUser = _.clone(dbUser._doc)
+    const refreshToken = resUser.accessToken
+    delete resUser.resetPassExpire
+    delete resUser.resetPassHash
+    delete resUser.password
+    delete resUser.iOSDeviceTokens
+    delete resUser.tOTPKey
+    delete resUser.__v
+    delete resUser.preferences
+    delete resUser.accessToken
+    delete resUser.deleted
+    delete resUser.hasL2Auth
 
-  const secret = nconf.get('tokens') ? nconf.get('tokens').secret : false
-  const expires = nconf.get('tokens') ? nconf.get('tokens').expires : 3600
-  if (!secret || !expires) return callback({ message: 'Invalid Server Configuration' })
+    const secret = nconf.get('tokens') ? nconf.get('tokens').secret : false
+    const expires = nconf.get('tokens') ? nconf.get('tokens').expires : 3600
+    
+    if (!secret || !expires) {
+      return reject(new Error('Invalid Server Configuration'))
+    }
 
-  require('../../models/group').getAllGroupsOfUserNoPopulate(dbUser._id, function (err, grps) {
-    if (err) return callback(err)
-    resUser.groups = grps.map(function (g) {
-      return g._id
+    require('../../models/group').getAllGroupsOfUserNoPopulate(dbUser._id, (err, grps) => {
+      if (err) return reject(err)
+      
+      resUser.groups = grps.map(function (g) {
+        return g._id
+      })
+
+      const token = jwt.sign({ user: resUser }, secret, { expiresIn: expires })
+      resolve({ token: token, refreshToken: refreshToken })
     })
-
-    const token = jwt.sign({ user: resUser }, secret, { expiresIn: expires })
-
-    return callback(null, { token: token, refreshToken: refreshToken })
   })
 }
 
